87c2018515933ce081debfb0551d99ac524d292a
nikita <yarosniks@gmail.com>
2024-09-07 00:01:43 +0700

    add "List"

--- laba1/list.cpp
+++ laba1/list.cpp
@@ -16,17 +16,6 @@ public:
 
     List(){}
 
-    void insert(int elem, int pos){
-        if (pos >= 100 || pos < 0){ cout << "List index out of range!\n";}
-        else{
-            for (int i = n - 1; i > pos; i++){
-                mass[i] = mass[i-1];
-            }
-        }
-    }
-
-    void remove(){}
-
     int indexOf(int item){
         if (n==0){
             return -1;
@@ -49,5 +38,56 @@ public:
         cout << endl;
     }
 
+    void insert(int elem, int pos){
+        if (pos >= 100 || pos < 0){ cout << "List index out of range!\n";}
+        else if (n==100){ cout << "List is overload!\n";}
+        else{
+            if (pos >= n){ insert(elem); }
+            else{
+                for (int i = n - 1; i > pos - 1; i--){
+                    mass[i+1] = mass[i];
+                }
+                mass[pos] = elem;
+                n++;
+            }
+        }
+    }
 
+    void insert(int elem){
+        if (n==100){ cout << "List is overload!\n";}
+        else{
+            mass[n] = elem;
+            n++;
+        }
+    }
+
+    void remove(int pos){
+        if (pos >= 100 || pos < 0){ cout << "List index out of range!\n";}
+        else if (isEmpty()){ cout << "List is empty!\n";}
+        else{
+            for (int i = pos; i < n; i++ ){
+                mass[i] = mass[i+1];
+            }
+            n--;
+        }
+    }
 };
+
+
+int main(){
+    List a;
+
+    a.insert(1);
+    a.insert(2);
+    a.insert(3);
+    a.insert(4);
+    a.print();
+    cout << a.indexOf(3) << endl;
+    a.remove(2);
+    a.print();
+    a.insert(5, 0);
+    a.insert(6, 0);
+    a.insert(7, 0);
+    a.print();
+
+}
